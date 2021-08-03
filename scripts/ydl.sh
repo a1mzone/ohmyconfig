b9711c44e36fb94df236937100779852aaee3688
Zak Coetzee <zakkcoetzee@gmail.com>
2021-08-03 22:23:54 +0200

    add ydl, sshr, sshrc, p aliase scripts

---
+++ scripts/ydl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# requires symbolic link to NEW folder in Music
+# downloads only the audio container
+# ln -s /mnt/c/Users/IZAKCOETZEE/Music ~/music
+
+cd ~/music/NEW/
+~/.local/bin/youtube-dl -f 140 'https://www.youtube.com/watch?v='$1
