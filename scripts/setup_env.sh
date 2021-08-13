d623fa2002db4281bebb4397274d6dba3f67ebd4
Zakk <zakk@localhost.localdomain>
2021-08-13 19:16:44 +0000

    updated zshrc and setup_env.sh

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 DISTRO=`lsb_release -i | cut -f 2-`
+DIR=`pwd`
+PARENT_DIR=`dirname $DIR`
+IAM=`whoami`
+
+echo "WORKING IN: $DIR"
+echo "PARENT DIR: $PARENT_DIR"
+echo ""
 
 if [ "$DISTRO" == "ManjaroLinux" ]; then
     echo "Manjaro"
@@ -8,32 +15,47 @@ if [ "$DISTRO" == "ManjaroLinux" ]; then
 
 elif [ "$DISTRO" = "Ubuntu" ]; then
     echo "Ubuntu"
-    #sudo apt install git zsh curl 
+    #sudo apt install git zsh curl
 
 fi
 
 # TODO - add yum / dnf support
 #sudo yum install zsh git curl
+test () {
+    grep -qxF 'alias src=source ~/.zshrc\' || echo "alias src='source ~/.zshrc'" >> ~/.zshrc
+}
 
 add_aliases () {
-    echo "alias src='source ~/.zshrc' \nalias esrc='vim ~/.zshrc'\n" >> ~/.zshrc \
-    echo "alias ns='sudo netstat -tulpn | grep LISTEN'" >> ~/.zshrc \
-    echo "alias pse='sudo ps -ef | grep'" >> ~/.zshrc \
-    echo "alias ss='sudo systemctl'" >> ~/.zshrc \
+    echo "alias src='source ~/.zshrc'" >> ~/.zshrc 
+    echo "alias esrc='vim ~/.zshrc'" >> ~/.zshrc 
+    echo "alias ns='sudo netstat -tulpn | grep LISTEN'" >> ~/.zshrc 
+    echo "alias pse='sudo ps -ef | grep'" >> ~/.zshrc 
+    echo "alias ss='sudo systemctl'" >> ~/.zshrc 
     echo "alias v='sudo vim'" >> ~/.zshrc
 }
 
+no_passwd_sudo () {
+    sudo grep -qxF '$IAM  ALL=(ALL) NOPASSWD: ALL' /etc/sudoers || sudo echo "$IAM  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers 
+}
 #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh_basics () {
+    sed -i 's/robbyrussell/gnzh/g' ~/.zshrc
+    sed -i 's/(git)/(git history)/g' ~/.zshrc
+}
 
-sed -i 's/robbyrussell/gnzh/g' ~/.zshrc 
-sed -i 's/(git)/(history)/g' ~/.zshrc
-
+python_virtual_env () {
+echo "# VIRTUALENV" >> ~/.zshrc
+echo "export WORKON_HOME=~/.virtualenvs" >> ~/.zshrc
+echo "mkdir -p $WORKON_HOME" >> ~/.zshrc
+echo "source /usr/bin/virtualenvwrapper.sh" >> ~/.zshrc
+}
 #echo "alias sai='sudo apt install' \nalias sau='sudo apt update && sudo apt upgrade'\n" >>  ~/.zshrc
 
-echo "Do you want to add aliases?"
+echo "Common aliases & gnzh & history plugin?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) add_aliases; break;;
+        #Yes ) add_aliases; zsh_basics; break;;
+        Yes ) no_passwd_sudo; break;;
         No )  break;;
     esac
 done
