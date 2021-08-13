3f189c9b1f8ef702c051139242a4787157a41bd8
Zakk Coetzee <zakkcoetzee@gmail.com>
2021-08-14 00:28:46 +0200

    cleaned up

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -13,6 +13,7 @@ zsh_basics () {
     sed -i 's/robbyrussell/gnzh/g' ~/.zshrc
     sed -i 's/(git)/(git history)/g' ~/.zshrc
 }
+
 # install ohmyzsh on Manjaro or Ubuntu
 install_ohmyzsh () {
     if [ "$DISTRO" == "ManjaroLinux" ]; then
@@ -28,24 +29,24 @@ install_ohmyzsh () {
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && zsh_basics
 }
 
-# TODO - add yum / dnf support
-#sudo yum install zsh git curl
+# test function
 check_test () {
     grep -qF "alias src='source ~/.zshrc'" $PARENT_DIR/zshrc_config || echo -e "alias src='source ~/.zshrc'" | tee -a $PARENT_DIR/zshrc_config
 }
 
+# add the current user to /etc/sudoers
 no_passwd_sudo () {
     sudo grep -qxF "${IAM}  ALL=(ALL) NOPASSWD: ALL" /etc/sudoers || echo -e "$IAM  ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers 
 }
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# setup python venvs
 python_virtual_env () {
     grep -qF "# VIRTUALENV" $PARENT_DIR/zshrc_config || echo -e "\n# VIRTUALENV" | tee -a $PARENT_DIR/zshrc_config
+    grep -qF "#export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" $PARENT_DIR/zshrc_config || echo -e "#export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" | tee -a $PARENT_DIR/zshrc_config 
     grep -qF "export WORKON_HOME=~/.virtualenvs" $PARENT_DIR/zshrc_config || echo -e "export WORKON_HOME=~/.virtualenvs" | tee -a $PARENT_DIR/zshrc_config
     grep -qF "mkdir -p $WORKON_HOME" $PARENT_DIR/zshrc_config || echo -e "mkdir -p $WORKON_HOME" | tee -a $PARENT_DIR/zshrc_config
     grep -qF "source /usr/bin/virtualenvwrapper.sh" $PARENT_DIR/zshrc_config || echo -e "source /usr/bin/virtualenvwrapper.sh" | tee -a $PARENT_DIR/zshrc_config
 }
-#echo "alias sai='sudo apt install' \nalias sau='sudo apt update && sudo apt upgrade'\n" >>  ~/.zshrc
 
 #echo ""
 #echo "install ohmyzsh [update required]?"
@@ -56,7 +57,7 @@ python_virtual_env () {
 #        No )  break;;
 #    esac
 #done
-#
+
 #echo ""
 #echo "add this user to /etc/sudoers?"
 #select yn in "Yes" "No"; do
@@ -67,12 +68,12 @@ python_virtual_env () {
 #    esac
 #done
 
-echo ""
-echo "python virualnenvwrapper?"
-select yn in "Yes" "No"; do
-    case $yn in
-        #Yes ) add_aliases; zsh_basics; break;;
-        Yes ) python_virtual_env; break;;
-        No )  break;;
-    esac
-done
+#echo ""
+#echo "python virualnenvwrapper?"
+#select yn in "Yes" "No"; do
+#    case $yn in
+#        #Yes ) add_aliases; zsh_basics; break;;
+#        Yes ) python_virtual_env; break;;
+#        No )  break;;
+#    esac
+#done
