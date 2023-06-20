cda0a16f0d94f7dfef668e71a562c5aeb7255260
Zakk Coetzee <zakkcoetzee@gmail.com>
2023-06-20 16:23:21 +0200

    fix youtube_dl

--- scripts/ydl.sh
+++ scripts/ydl.sh
@@ -3,6 +3,7 @@
 # requires symbolic link to NEW folder in Music
 # downloads only the audio container
 # ln -s /mnt/c/Users/IZAKCOETZEE/Music ~/music
+# using https://github.com/yt-dlp/yt-dlp since youtube_dl pip is stale
 
 cd ~/Music/NEW/
-~/.local/bin/youtube-dl -f 140 'https://www.youtube.com/watch?v='$1
+/usr/local/bin/yt-dlp -f 140 'https://www.youtube.com/watch?v='$1
